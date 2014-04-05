64f9b72f8dcc6b92b04e3539296548796dfc9c6f
arkzyna <arkzyna@gmail.com>
2014-04-05 12:13:09 +0200

    dijkstra

--- second-day/perrine.py
+++ second-day/perrine.py
@@ -14,7 +14,10 @@ source =
 
 length = []
 l_path = []
-for vertice in graph.vertices :
-	a,b = bidirectional_dijkstra(graph, source, vertice, weight='weight')
+for vertice in graph.nodes() :
+	#change all length so that it becomes a minimisation problem : length = 1/length
+	a,b = bidirectional_dijkstra(graph, source, vertice, weight='length')
+	c,d = bidirectional_dijkstra(graph, source, vertice, weight='time')
 	length.append(a)
 	l_path.append(b)
+
