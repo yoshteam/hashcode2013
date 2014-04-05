385c45a931b6288aff7867686707a754fd4cefe3
arkzyna <arkzyna@gmail.com>
2014-04-05 11:58:46 +0200

    dijkstra

---
+++ second-day/perrine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# sam1.py
+# Perrine Letellier
+#
+
+import numpy as np
+from scipy import ndimage
+import networkx as nx
+
+
+graph = 
+source =
+
+for vertice in graph.vertices :
+	bidirectional_dijkstra(graph, source, vertice, weight='weight')
