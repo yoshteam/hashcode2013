b3ec50e295a98aea25b46fbb25ee17e3a3306316
Sergey Kirgizov <sergey.kirgizov@lip6.fr>
2014-04-05 08:21:34 +0200

    49969

--- trial/3.py
+++ trial/3.py
@@ -10,11 +10,12 @@ f=open("sol","w")
 
 input = deepcopy(A)
 
-for s in range(30,0,-1): 
+for s in range(25,-1,-1): 
   print (s)
-  for x in range(s,n-1-s):
-    for y in range(s,m-1-s):
-      if (np.all(input[(x-s):(x+s),(y-s):(y+s)]) == 1):
+  for x in range(s,n-s):
+    for y in range(s,m-s):
+      if (np.all(input[(x-s):(x+s+1),(y-s):(y+s+1)] == 1)):
+#        print (x-s),(x+s),(y-s),(y+s)
         print >>f, "PAINTSQ", x, y, s
         print "PAINTSQ", x, y, s
         input[(x-s):(x+s),(y-s):(y+s)] = 0
